9366bd6e1dd7f06fec65f9d4a2abfc20cd856990
piotr-sekula <psekula@virtuslab.com>
2023-05-18 15:09:32 +0200

    Import project from spring initializer

---
+++ src/main/java/com/virtuslab/vssjavastats/VssjavastatsApplication.java
@@ -0,0 +1,13 @@
+package com.virtuslab.vssjavastats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VssjavastatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VssjavastatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtuslab/vssjavastats/VssjavastatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtuslab.vssjavastats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VssjavastatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
