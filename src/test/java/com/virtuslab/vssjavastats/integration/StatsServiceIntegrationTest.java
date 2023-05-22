8675515aafa2e54c6d089f092a50c587079f4d3d
piotr-sekula <psekula@virtuslab.com>
2023-05-22 13:54:43 +0200

    Add integration test for stats endpoint

--- src/test/java/com/virtuslab/vssjavastats/integration/StatsServiceIntegrationTest.java
+++ src/test/java/com/virtuslab/vssjavastats/integration/StatsServiceIntegrationTest.java
@@ -1,8 +1,10 @@
 package com.virtuslab.vssjavastats.integration;
 
+import com.virtuslab.vssjavastats.controller.HashTypesView;
 import com.virtuslab.vssjavastats.domain.Password;
 import com.virtuslab.vssjavastats.domain.StatsRepository;
 import com.virtuslab.vssjavastats.events.KafkaEventConsumer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,14 +15,26 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ContextConfiguration(initializers = {StoreEventIntegrationTest.Initializer.class})
-public class StoreEventIntegrationTest extends AbstractIntegrationTest {
+@ContextConfiguration(initializers = {StatsServiceIntegrationTest.Initializer.class})
+public class StatsServiceIntegrationTest extends AbstractIntegrationTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
 
     @Autowired
     private StatsRepository statsRepository;
@@ -38,6 +52,11 @@ public class StoreEventIntegrationTest extends AbstractIntegrationTest {
         }
     }
 
+    @BeforeAll
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
     @Test
     void shouldStoreEventFromKafkaInDatabase() {
         // when
@@ -48,5 +67,28 @@ public class StoreEventIntegrationTest extends AbstractIntegrationTest {
         assertEquals("452933f447fe6aaa46cae4860239ca72", result.hash());
         assertEquals("MD5", result.hashType());
         assertEquals("123!@#abc", result.password());
+    }
+
+    @Test
+    void shouldReturnStatsFromDedicatedEndpoint() throws Exception {
+        //given
+        statsRepository.save(new Password("MD5", "abc", "!@#"));
+        statsRepository.save(new Password("MD5", "def", "!@#"));
+        statsRepository.save(new Password("SHA-1", "ghi", "!@#"));
+        statsRepository.save(new Password("SHA-1", "jkl", "!@#"));
+        statsRepository.save(new Password("SHA-1", "mno", "!@#"));
+        statsRepository.save(new Password("test", "prs", "!@#"));
+
+        // when
+        this.mockMvc
+                .perform(get("/stats"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", hasSize(3)))
+                .andExpect(jsonPath("$.data[0].type", is("MD5")))
+                .andExpect(jsonPath("$.data[0].amount", is(2)))
+                .andExpect(jsonPath("$.data[1].type", is("SHA-1")))
+                .andExpect(jsonPath("$.data[1].amount", is(3)))
+                .andExpect(jsonPath("$.data[2].type", is("test")))
+                .andExpect(jsonPath("$.data[2].amount", is(1)));
     }
 }
